6802bbf33021b01cd2895f981c39e99fe620e9da
Ekaterina <ekaterina.balakina@myget-it.com>
2020-08-30 23:45:10 +0300

    Add api usage

--- src/components/Map/MapContainer.jsx
+++ src/components/Map/MapContainer.jsx
@@ -4,40 +4,56 @@ import HeatmapLayer from "react-leaflet-heatmap-layer";
 
 import * as buildings from "../../data/csvjson.json";
 import { getRandomInt } from "../../utils";
+import { getMap } from "../../api/mapApi";
+import { useQuery } from "react-query";
 
-const MapContainer = ({ points, center }) => (
-  <Map center={center} zoom={12}>
-    <TileLayer
-      url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-      attribution="&copy; <a href=http://osm.org/copyright>OpenStreetMap</a> contributors"
-    />
-    <LayersControl>
-      <LayersControl.Overlay name="Плотность" checked>
-        <FeatureGroup color="purple">
-          <HeatmapLayer
-            fitBoundsOnLoad
-            fitBoundsOnUpdate
-            points={points}
-            longitudeExtractor={(point) => point.center_lng}
-            latitudeExtractor={(point) => point.center_lat}
-            intensityExtractor={() => getRandomInt(1, 30)}
-          />
-        </FeatureGroup>
-      </LayersControl.Overlay>
-      <LayersControl.Overlay name="Конкуренты">
-        <FeatureGroup color="purple">
-          <HeatmapLayer
-            fitBoundsOnLoad
-            fitBoundsOnUpdate
-            points={buildings.default}
-            longitudeExtractor={(point) => point.center_lng}
-            latitudeExtractor={(point) => point.center_lat}
-            intensityExtractor={() => 1}
-          />
-        </FeatureGroup>
-      </LayersControl.Overlay>
-    </LayersControl>
-  </Map>
-);
+const MapContainer = ({ points, center }) => {
+  const { isLoading, error, data } = useQuery("fetchMapData", getMap);
+
+  if (isLoading) {
+    return "Loading...";
+  }
+
+  if (error) {
+    return "Error" + error.message;
+  }
+
+  console.log(data);
+
+  return (
+    <Map center={center} zoom={12}>
+      <TileLayer
+        url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        attribution="&copy; <a href=http://osm.org/copyright>OpenStreetMap</a> contributors"
+      />
+      <LayersControl>
+        <LayersControl.Overlay name="Плотность" checked>
+          <FeatureGroup color="purple">
+            <HeatmapLayer
+              fitBoundsOnLoad
+              fitBoundsOnUpdate
+              points={points}
+              longitudeExtractor={(point) => point.center_lng}
+              latitudeExtractor={(point) => point.center_lat}
+              intensityExtractor={() => getRandomInt(1, 30)}
+            />
+          </FeatureGroup>
+        </LayersControl.Overlay>
+        <LayersControl.Overlay name="Конкуренты">
+          <FeatureGroup color="purple">
+            <HeatmapLayer
+              fitBoundsOnLoad
+              fitBoundsOnUpdate
+              points={buildings.default}
+              longitudeExtractor={(point) => point.center_lng}
+              latitudeExtractor={(point) => point.center_lat}
+              intensityExtractor={() => 1}
+            />
+          </FeatureGroup>
+        </LayersControl.Overlay>
+      </LayersControl>
+    </Map>
+  );
+};
 
 export default MapContainer;
